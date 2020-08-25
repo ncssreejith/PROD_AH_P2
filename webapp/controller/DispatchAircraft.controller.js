8e324982aeef846216b7eb5f3be161d30325fc22
furyx <furyx@hotmail.com>
2020-08-25 17:43:55 +0800

    Teck Meng 25/08 17:34 Plaform transfer bug fixes. S/N 25, 26, 27

--- webapp/controller/DispatchAircraft.controller.js
+++ webapp/controller/DispatchAircraft.controller.js
@@ -6,14 +6,24 @@ sap.ui.define([
 	"../model/formatter",
 	"../util/ajaxutil",
 	"sap/ui/model/json/JSONModel",
-	"sap/m/MessageBox"
-], function(BaseController, dataUtil, Fragment, FieldValidations, formatter, ajaxutil, JSONModel, MessageBox) {
+	"sap/m/MessageBox",
+	"sap/base/Log"
+], function(BaseController, dataUtil, Fragment, FieldValidations, formatter, ajaxutil, JSONModel, MessageBox, Log) {
 	"use strict";
 	/* ***************************************************************************
-	 *   This file is for ???????            
-	 *   1. Purpose for this file ????
-	 *	Note: ??????????
-	 * IMPORTANT : Must give documentation for all functions
+	 *	 Developer : KUMAR AMIT	
+	 *   Control name: Station           
+	 *   Purpose : Display of Dispatch aircraft  and fly log and mano
+	 *   Functions : 
+	 *   1.UI Events
+	 *		1.1 onInit
+	 *		1.2 onSignOffPress
+	 *	 2. Backend Calls
+	 *		2.1 fnLogById
+	 *	 3. Private calls
+	 *		3.1 _onObjectMatched
+	 *		3.2 fnSetReason
+	 *   Note : 
 	 *************************************************************************** */
 	return BaseController.extend("avmet.ah.controller.DispatchAircraft", {
 		formatter: formatter,
@@ -21,29 +31,39 @@ sap.ui.define([
 		//                 1. UI Events  
 		// ***************************************************************************
 		onInit: function() {
-			this.getRouter().getRoute("DispatchAircraft").attachPatternMatched(this._onObjectMatched, this);
-			var oatchkData = {};
-			oatchkData.isListChange = "";
-			oatchkData.header = {};
-			oatchkData.checklist = [];
-			oatchkData.location = [];
-			oatchkData.sqn = [];
-			this.setModel(new JSONModel(), "atckModel");
-			this.setModel(new JSONModel(), "oViewModel");
+			try {
+				this.getRouter().getRoute("DispatchAircraft").attachPatternMatched(this._onObjectMatched, this);
+				var oatchkData = {};
+				oatchkData.isListChange = "";
+				oatchkData.header = {};
+				oatchkData.checklist = [];
+				oatchkData.location = [];
+				oatchkData.sqn = [];
+				this.setModel(new JSONModel(), "atckModel");
+				this.setModel(new JSONModel(), "oViewModel");
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:onInit function");
+				this.handleException(e);
+			}
 		},
 		// ***************************************************************************
 		//                 2. Database/Ajax/OData Calls  
 		// ***************************************************************************	
 		//1.on click of ico selected
 		onIconTabSelection: function(oEvent) {
-			var key = oEvent.getSource().getSelectedKey(),
-				oModel = this.getView().getModel("oViewModel");
-			if (key === "receiver") {
-				oModel.setProperty("/Dispatch", false);
-				oModel.setProperty("/Receive", true);
-			} else {
-				oModel.setProperty("/Dispatch", true);
-				oModel.setProperty("/Receive", false);
+			try {
+				var key = oEvent.getSource().getSelectedKey(),
+					oModel = this.getView().getModel("oViewModel");
+				if (key === "receiver") {
+					oModel.setProperty("/Dispatch", false);
+					oModel.setProperty("/Receive", true);
+				} else {
+					oModel.setProperty("/Dispatch", true);
+					oModel.setProperty("/Receive", false);
+				}
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:onIconTabSelection function");
+				this.handleException(e);
 			}
 		},
 		/*onDispatch: function (oEvent) {
@@ -54,51 +74,80 @@ sap.ui.define([
 		},*/
 		//2.on dispatch of the aircraft
 		onDispatchClick: function(oEvt) {
-			var DBInitialModel = {
-				"StatusTitle": "Awaiting Recipient",
-				"StatusButton": "Receive Aircraft",
-				"StatusHeader": "This aircraft is undergoing transfer",
-				"StatusSubHeader": "",
-				"StatusInfo": "AH C 610 is locked"
-			};
-			dataUtil.setDataSet("DBInitialModel", DBInitialModel);
-			this.getOwnerComponent().getRouter().navTo("DashboardInitial");
+			try {
+				var DBInitialModel = {
+					"StatusTitle": "Awaiting Recipient",
+					"StatusButton": "Receive Aircraft",
+					"StatusHeader": "This aircraft is undergoing transfer",
+					"StatusSubHeader": "",
+					"StatusInfo": "AH C 610 is locked"
+				};
+				dataUtil.setDataSet("DBInitialModel", DBInitialModel);
+				this.getOwnerComponent().getRouter().navTo("DashboardInitial");
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:onDispatchClick function");
+				this.handleException(e);
+			}
 		},
 		//3.on click of the received aircraft
 		onReceive: function(oEvent) {
-			var DBInitialModel = {
-				"StatusTitle": "Serviceable",
-				"StatusButton": "noButton",
-				"StatusHeader": "This aircraft is Serviceable",
-				"StatusSubHeader": "There are no outstanding nor overdue jobs.",
-				"StatusInfo": ""
-			};
-			dataUtil.setDataSet("DBInitialModel", DBInitialModel);
-			this.getOwnerComponent().getRouter().navTo("DashboardInitial");
+			try {
+				var DBInitialModel = {
+					"StatusTitle": "Serviceable",
+					"StatusButton": "noButton",
+					"StatusHeader": "This aircraft is Serviceable",
+					"StatusSubHeader": "There are no outstanding nor overdue jobs.",
+					"StatusInfo": ""
+				};
+				dataUtil.setDataSet("DBInitialModel", DBInitialModel);
+				this.getOwnerComponent().getRouter().navTo("DashboardInitial");
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:onReceive function");
+				this.handleException(e);
+			}
 		},
 		//4.on selection change of location combobox
 		onSelectionChangeLoc: function(oEvent) {
-			var key = oEvent.getSource().getSelectedKey(),
-				oModel = this.getView().getModel("oViewModel");
-			if (key !== "") {
-				oModel.setProperty("/sReceiveTable", true);
-			} else {
-				oModel.setProperty("/sReceiveTable", false);
+			try {
+				var key = oEvent.getSource().getSelectedKey(),
+					oModel = this.getView().getModel("oViewModel");
+				if (key !== "") {
+					oModel.setProperty("/sReceiveTable", true);
+				} else {
+					oModel.setProperty("/sReceiveTable", false);
+				}
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:onSelectionChangeLoc function");
+				this.handleException(e);
 			}
 		},
 		onSelectionChangesqn: function(oEvent) {
-			var sText = oEvent.getSource()._getSelectedItemText(),
-				oModel = this.getView().getModel("atckModel");
+			try {
+				var sText = oEvent.getSource()._getSelectedItemText(),
+					oModel = this.getView().getModel("atckModel");
 				oModel.setProperty("/header/RSQN", sText);
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:onSelectionChangesqn function");
+				this.handleException(e);
+			}
 		},
 		//5.to enable dispatch based on the selection of custodian
 		onSelecectionCustodian: function(oEvent) {
-			this.getModel("atckModel").setProperty("/isListChange", true);
-			this.getModel("atckModel").refresh();
-
+			try {
+				this.getModel("atckModel").setProperty("/isListChange", true);
+				this.getModel("atckModel").refresh();
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:onSelecectionCustodian function");
+				this.handleException(e);
+			}
 		},
 		onNavBackDispatch: function() {
-			this.getRouter().navTo("AircraftTransfer");
+			try {
+				this.getRouter().navTo("AircraftTransfer");
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:onNavBackDispatch function");
+				this.handleException(e);
+			}
 		},
 
 		// ***************************************************************************
@@ -108,61 +157,86 @@ sap.ui.define([
 		//                 4. Private Methods   
 		// ***************************************************************************
 		_onObjectMatched: function(oEvent) {
-			this.fnLoadHeader();
-			this.fnLoadCheckList();
-			this.fnLoadLocation();
-			this.fnLoadSqn();
+			try {
+				this.fnLoadHeader();
+				this.fnLoadCheckList();
+				this.fnLoadLocation();
+				this.fnLoadSqn();
+				var oPage = this.getView().byId("pageDispatchId"); //Get Hold of page
+				oPage.scrollTo(0, 0); //Page scroll to top
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:_onObjectMatched function");
+				this.handleException(e);
+			}
 		},
+		/** 
+		 * Load current SQN header
+		 */
 		fnLoadHeader: function() {
-			var sPath = "/airtranscurrsvc/" + this.getTailId();
-			var oParameter = {};
-			oParameter.error = function() {};
-			oParameter.success = function(oData) {
-				var oHeader = oData.results.length > 0 ? oData.results[0] : {};
-				if(oHeader.SQN){
-					oHeader.RSQN = oHeader.SQN;
-				}
-				this.getModel("atckModel").setProperty("/header", oHeader);
-				var oModel = this.getView().getModel("oViewModel");
-				if (this.getModel("atckModel").getProperty("/header/ASTID")) {
-					oModel.setProperty("/Dispatch", false);
-					oModel.setProperty("/Receive", true);
-				} else {
-					oModel.setProperty("/Dispatch", true);
-					oModel.setProperty("/Receive", false);
-				}
-				this.getModel("atckModel").refresh(true);
-			}.bind(this);
-			ajaxutil.fnRead(sPath, oParameter);
+			try {
+				var sPath = "/airtranscurrsvc/" + this.getTailId();
+				var oParameter = {};
+				oParameter.error = function() {};
+				oParameter.success = function(oData) {
+					var oHeader = oData.results.length > 0 ? oData.results[0] : {};
+					if (oHeader.SQN) {
+						oHeader.RSQN = oHeader.SQN;
+					}
+					this.getModel("atckModel").setProperty("/header", oHeader);
+					var oModel = this.getView().getModel("oViewModel");
+					if (this.getModel("atckModel").getProperty("/header/ASTID")) {
+						oModel.setProperty("/Dispatch", false);
+						oModel.setProperty("/Receive", true);
+					} else {
+						oModel.setProperty("/Dispatch", true);
+						oModel.setProperty("/Receive", false);
+					}
+					this.getModel("atckModel").refresh(true);
+				}.bind(this);
+				ajaxutil.fnRead(sPath, oParameter);
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:fnLoadHeader function");
+				this.handleException(e);
+			}
 		},
 
 		fnLoadCheckList: function() {
-			var sPath = "/ATCheckListSvc";
-			var oParameter = {};
-			oParameter.filter = "airid eq " + this.getAircraftId() + " and tailid eq " + this.getTailId();
-			oParameter.error = function() {
+			try {
+				var sPath = "/ATCheckListSvc";
+				var oParameter = {};
+				oParameter.filter = "airid eq " + this.getAircraftId() + " and tailid eq " + this.getTailId();
+				oParameter.error = function() {
 
-			};
-			oParameter.success = function(oData) {
-				oData.results.forEach(function(oItem) {
-					oItem.answ = oItem.answ === "" ? "NA" : oItem.answ;
-					oItem.answ1 = oItem.answ1 === "Y" ? true : false;
-				});
-				this.getModel("atckModel").setProperty("/checklist", oData.results);
-				this.getModel("atckModel").refresh(true);
-			}.bind(this);
-			ajaxutil.fnRead(sPath, oParameter);
+				};
+				oParameter.success = function(oData) {
+					oData.results.forEach(function(oItem) {
+						oItem.answ = oItem.answ === "" ? "NA" : oItem.answ;
+						oItem.answ1 = oItem.answ1 === "Y" ? true : false;
+					});
+					this.getModel("atckModel").setProperty("/checklist", oData.results);
+					this.getModel("atckModel").refresh(true);
+				}.bind(this);
+				ajaxutil.fnRead(sPath, oParameter);
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:fnLoadCheckList function");
+				this.handleException(e);
+			}
 		},
 		fnLoadSquadranList: function() {
-			var sPath = "/airtranssvc";
-			var oParameter = {};
-			oParameter.error = function() {
+			try {
+				var sPath = "/airtranssvc";
+				var oParameter = {};
+				oParameter.error = function() {
 
-			};
-			oParameter.success = function(oData) {
-				this.getModel("atckModel").setProperty("/checklist", oData.results);
-			}.bind(this);
-			ajaxutil.fnRead(sPath, oParameter);
+				};
+				oParameter.success = function(oData) {
+					this.getModel("atckModel").setProperty("/checklist", oData.results);
+				}.bind(this);
+				ajaxutil.fnRead(sPath, oParameter);
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:fnLoadSquadranList function");
+				this.handleException(e);
+			}
 		},
 
 		// 	CSQN: "140 SQN"
@@ -174,78 +248,102 @@ sap.ui.define([
 		// SQN: "140 SQN"
 		// SQNID: "SQN_140"
 		fnCheckResponse: function() {
-			var bCheckFail = false;
-			this.getModel("atckModel").getProperty("/checklist").forEach(function(oItem) {
-				if (bCheckFail) {
-					return;
-				}
-				if (oItem.answ === "Y" && !oItem.answ1) {
-					MessageBox.error("Please align with current checklist.");
-					bCheckFail = true;
-				}
-			});
+			try {
+				var bCheckFail = false;
+				this.getModel("atckModel").getProperty("/checklist").forEach(function(oItem) {
+					if (bCheckFail) {
+						return;
+					}
+					if (oItem.answ === "Y" && !oItem.answ1) {
+						MessageBox.error("Please align with current checklist.");
+						bCheckFail = true;
+					}
+				});
 
-			return bCheckFail;
+				return bCheckFail;
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:fnCheckResponse function");
+				this.handleException(e);
+			}
 		},
 
+		/** 
+		 * Signoff Function
+		 * @returns
+		 */
 		fnSubmitResponse: function() {
-			var oData = [];
-			var oModel = this.getView().getModel("oViewModel");
+			try {
+				var oData = [];
+				var oModel = this.getView().getModel("oViewModel");
 
-			if (oModel.getProperty("/Receive") && this.fnCheckResponse()) {
-				return; //Check fail
-			}
-			this.getModel("atckModel").getProperty("/checklist").forEach(function(oItem) {
-				var oPayload = {};
-				oPayload.tranid = this.getModel("atckModel").getProperty("/isListChange") ? "X" : "";
-				oPayload.chng = this.getModel("atckModel").getProperty("/isListChange") ? "X" : "";
-				oPayload.tailid = this.getTailId();
-				oPayload.airid = this.getAircraftId();
-				oPayload.listid = oItem.listid;
-				oPayload.transt = this.getModel("atckModel").getProperty("/header/ASTID") === '' ? "D" : "R";
-				oPayload.csqnid = this.getModel("atckModel").getProperty("/header/CSQNID");
-				oPayload.sqnid = this.getModel("atckModel").getProperty("/header/SQNID"); //oPayload.transt==='D'?this.getModel("atckModel").getProperty("/header/SQNID"):this.getModel("atckModel").getProperty("/header/CSQNID");
-				oPayload.locid = this.getModel("atckModel").getProperty("/header/LOCID");
-				oPayload.chkid = oItem.chkid;
-				oPayload.chkdesc = oItem.chkdesc;
-				oPayload.answ = oItem.answ;
-				oPayload.answ1 = oItem.answ1 ? "Y" : "NA";
-				oData.push(oPayload);
-			}.bind(this));
-			var oParameter = {};
-			oParameter.activity = 1;
-			oParameter.error = function() {
+				if (oModel.getProperty("/Receive") && this.fnCheckResponse()) {
+					return; //Check fail
+				}
+				this.getModel("atckModel").getProperty("/checklist").forEach(function(oItem) {
+					var oPayload = {};
+					oPayload.tranid = this.getModel("atckModel").getProperty("/isListChange") ? "X" : "";
+					oPayload.chng = this.getModel("atckModel").getProperty("/isListChange") ? "X" : "";
+					oPayload.tailid = this.getTailId();
+					oPayload.airid = this.getAircraftId();
+					oPayload.listid = oItem.listid;
+					oPayload.transt = this.getModel("atckModel").getProperty("/header/ASTID") === '' ? "D" : "R";
+					oPayload.csqnid = this.getModel("atckModel").getProperty("/header/CSQNID");
+					oPayload.sqnid = this.getModel("atckModel").getProperty("/header/SQNID"); //oPayload.transt==='D'?this.getModel("atckModel").getProperty("/header/SQNID"):this.getModel("atckModel").getProperty("/header/CSQNID");
+					oPayload.locid = this.getModel("atckModel").getProperty("/header/LOCID");
+					oPayload.chkid = oItem.chkid;
+					oPayload.chkdesc = oItem.chkdesc;
+					oPayload.answ = oItem.answ;
+					oPayload.answ1 = oItem.answ1 ? "Y" : "NA";
+					oData.push(oPayload);
+				}.bind(this));
+				var oParameter = {};
+				oParameter.activity = 1;
+				oParameter.error = function() {
 
-			};
-			oParameter.success = function(oData) {
-				// this.getModel("atckModel").setProperty("/checklist",oData.results);
-				this.onDispatchClick();
-				this.getRouter().navTo("DashboardInitial");
-			}.bind(this);
+				};
+				oParameter.success = function(oData) {
+					// this.getModel("atckModel").setProperty("/checklist",oData.results);
+					this.onDispatchClick();
+					this.getRouter().navTo("DashboardInitial");
+				}.bind(this);
 
-			if (oData) {
-				ajaxutil.fnCreate("/ATCheckListSvc", oParameter, oData, "ZRM_TR_AC", this);
+				if (oData) {
+					ajaxutil.fnCreate("/ATCheckListSvc", oParameter, oData, "ZRM_TR_AC", this);
+				}
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:fnSubmitResponse function");
+				this.handleException(e);
 			}
 		},
 		fnLoadLocation: function() {
-			var oParameter = {};
-			oParameter.filter = "REFID eq " + this.getAircraftId() + " and LFLAG eq L";
-			oParameter.error = function() {};
-			oParameter.success = function(oData) {
-				this.getModel("atckModel").setProperty("/location", oData.results);
-				this.getModel("atckModel").refresh();
-			}.bind(this);
-			ajaxutil.fnRead("/airtranscurrsvc", oParameter);
+			try {
+				var oParameter = {};
+				oParameter.filter = "REFID eq " + this.getAircraftId() + " and LFLAG eq L";
+				oParameter.error = function() {};
+				oParameter.success = function(oData) {
+					this.getModel("atckModel").setProperty("/location", oData.results);
+					this.getModel("atckModel").refresh();
+				}.bind(this);
+				ajaxutil.fnRead("/airtranscurrsvc", oParameter);
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:fnLoadLocation function");
+				this.handleException(e);
+			}
 		},
 		fnLoadSqn: function() {
-			var oParameter = {};
-			oParameter.filter = "REFID eq " + this.getAircraftId() + " and LFLAG eq S";
-			oParameter.error = function() {};
-			oParameter.success = function(oData) {
-				this.getModel("atckModel").setProperty("/sqn", oData.results);
-				this.getModel("atckModel").refresh();
-			}.bind(this);
-			ajaxutil.fnRead("/airtranscurrsvc", oParameter);
+			try {
+				var oParameter = {};
+				oParameter.filter = "REFID eq " + this.getAircraftId() + " and LFLAG eq S";
+				oParameter.error = function() {};
+				oParameter.success = function(oData) {
+					this.getModel("atckModel").setProperty("/sqn", oData.results);
+					this.getModel("atckModel").refresh();
+				}.bind(this);
+				ajaxutil.fnRead("/airtranscurrsvc", oParameter);
+			} catch (e) {
+				Log.error("Exception in DispatchAircraft:fnLoadSqn function");
+				this.handleException(e);
+			}
 		}
 
 	});
