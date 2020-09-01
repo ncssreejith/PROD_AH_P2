360adcf5e0a583e57f3a2aa6b8950d362ca8aa58
sreejith.ravindran@ncs.com.sg <sreejith.ravindran@ncs.com.sg>
2020-09-01 20:26:58 +0800

    Rahul: COS: Limitations :01/09/2020 PM

--- webapp/controller/LimitationsOverView.controller.js
+++ webapp/controller/LimitationsOverView.controller.js
@@ -433,7 +433,7 @@ sap.ui.define([
 
 					}*/
 					if (oData && oData.results.length > 0) {
-						if (oData.results[0].EXTEND !== '' && oData.results[0].EXTEND !== null) {
+						if (oData.results[0].EXTEND !== '' || oData.results[0].EXTEND !== null) {
 							this._fnADDCapDataMultipleGet("E", sJobId, sCapId);
 							oModel.setProperty("/tableFlag", true);
 						}
