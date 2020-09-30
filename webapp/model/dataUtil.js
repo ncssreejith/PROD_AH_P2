5a6db90e2931bfe68f24098a780674e4c68bf61c
rajat <sreejith.ravindran@ncs.com.sg>
2020-09-30 18:27:54 +0800

    Rajat, 30 Sep, 6.27pm Session changes

--- webapp/model/dataUtil.js
+++ webapp/model/dataUtil.js
@@ -1,59 +1,84 @@
 sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/Device"
-], function (JSONModel, Device) {
+], function(JSONModel, Device) {
 	"use strict";
 	/* *******************************************************************************************************************
-    *   This file is for Managing Database/Ajax/OData Calls               
-    *   1. getDataSet : To getting the saved data from local storage. 
-    *                   eg: Previously if the data for Table 'POTable' have saved as 'POTableData', 
-    *					you will get the saved data by calling getDataSet('POTableData') from controller
-    *	2. setDataSet : If you have a dataset in view which you have to save to backed,use this (as per now it will save locally) 
-    *                   eg: If you have a dataset 'oPOTableData' for the Table 'POTable',then send the data as 
-    *					setDataSet('POTableData',oPOTableData) from controller
-    *   3. getJsonModel : If you wanted to get the saved dataset as JSON model, instead of calling getDataSet, use this method.
-    *					  It will give you the JSON model insead of data.
-    *	4. createJsonModel : If you wanted to create JSON model, always use this function instead of writting it in controller.
-    *						 This will help us to find JSON model usage in entire application 
-     ******************************************************************************************************************* */
+	 *   This file is for Managing Database/Ajax/OData Calls               
+	 *   1. getDataSet : To getting the saved data from local storage. 
+	 *                   eg: Previously if the data for Table 'POTable' have saved as 'POTableData', 
+	 *					you will get the saved data by calling getDataSet('POTableData') from controller
+	 *	2. setDataSet : If you have a dataset in view which you have to save to backed,use this (as per now it will save locally) 
+	 *                   eg: If you have a dataset 'oPOTableData' for the Table 'POTable',then send the data as 
+	 *					setDataSet('POTableData',oPOTableData) from controller
+	 *   3. getJsonModel : If you wanted to get the saved dataset as JSON model, instead of calling getDataSet, use this method.
+	 *					  It will give you the JSON model insead of data.
+	 *	4. createJsonModel : If you wanted to create JSON model, always use this function instead of writting it in controller.
+	 *						 This will help us to find JSON model usage in entire application 
+	 ******************************************************************************************************************* */
 	return {
 		/* Function : getDataSet
-		*  parameter : vDataSetName
-		*  To get the data from local storage
-		*/
-		getDataSet: function(vDataSetName){
-			if(this._storage === undefined)
-			this._storage = new jQuery.sap.storage.Storage(jQuery.sap.storage.Type.local);
+		 *  parameter : vDataSetName
+		 *  To get the data from local storage
+		 */
+		getDataSet: function(vDataSetName) {
+			if (this._storage === undefined) {
+				this._storage = new jQuery.sap.storage.Storage(jQuery.sap.storage.Type.local);
+			}
 			var oDataSet = this._storage.get(vDataSetName);
-			return this._storage.get(vDataSetName)===null? null:oDataSet;
+			var info = this._storage.get(vDataSetName) === null ? null : oDataSet;
+			return this._decryptInfo(info);
 		},
 		/* Function : setDataSet
-		*  parameter : vDataSetName
-		*  parameter : oDataSet
-		*  To set the data from local storage
-		*/
-		setDataSet: function(vDataSetName,oDataSet){
+		 *  parameter : vDataSetName
+		 *  parameter : oDataSet
+		 *  To set the data from local storage
+		 */
+		setDataSet: function(vDataSetName, oDataSet) {
 			this._storage = new jQuery.sap.storage.Storage(jQuery.sap.storage.Type.local);
-			this._storage.put(vDataSetName, oDataSet);
+			this._storage.put(vDataSetName, this._encriptInfo(oDataSet));
 			return true;
 		},
 		/* Function : getJsonModel
-		*  parameter : vDataSetName
-		*  To get Json model by passing data set
-		*/
-		getJsonModel: function(vDataSetName){
+		 *  parameter : vDataSetName
+		 *  To get Json model by passing data set
+		 */
+		getJsonModel: function(vDataSetName) {
 			return this.createJsonModel(this.getDataSet(vDataSetName));
 		},
 		/* Function : createJsonModel
-		*  parameter : oDataSet
-		*  To create Json model by passing data set
-		*/
-		createJsonModel: function(oDataSet){
-			return (oDataSet!== undefined && oDataSet!== null)?new sap.ui.model.json.JSONModel(oDataSet):null;
+		 *  parameter : oDataSet
+		 *  To create Json model by passing data set
+		 */
+		createJsonModel: function(oDataSet) {
+			return (oDataSet !== undefined && oDataSet !== null) ? new sap.ui.model.json.JSONModel(oDataSet) : null;
 		},
-		createNewJsonModel: function () {
+		createNewJsonModel: function() {
 			return new JSONModel();
 		},
+
+		fnGetUIEncriptionKey: function(info) {
+			return '8Cnh7Ks5Mp'; // Later will replace with service call
+		},
+		_encriptInfo: function(info) {
+			if (info) {
+				info = btoa(btoa(JSON.stringify(info)) + this.fnGetUIEncriptionKey());
+				return info;
+			}
+			return null;
+
+		},
+
+		_decryptInfo: function(info) {
+			if (info) {
+				info = atob(info);
+				info = info.replace(this.fnGetUIEncriptionKey(), '');
+				info = atob(info);
+				return JSON.parse(info);
+			}
+			return null;
+
+		}
 
 	};
 });
